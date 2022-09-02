27383041f0e8e04d05c3bae593e25613201be09d
Aldenivan Junior <aldenivan1207@gmail.com>
2022-09-01 21:09:03 -0300

    feat: constructuring constroller and view base to run the project

---
+++ Controllers/PlacesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SinqiaTest.DB;
+using SinqiaTest.Model;
+
+namespace SinqiaTest.Controllers
+{
+    public class PlacesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public PlacesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Places
+        public async Task<IActionResult> Index()
+        {
+              return _context.Places != null ? 
+                          View(await _context.Places.ToListAsync()) :
+                          Problem("Entity set 'DataContext.Places'  is null.");
+        }
+
+        // GET: Places/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Places == null)
+            {
+                return NotFound();
+            }
+
+            var place = await _context.Places
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+
+            return View(place);
+        }
+
+        // GET: Places/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Places/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,State,City,Date")] Place place)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(place);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(place);
+        }
+
+        // GET: Places/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Places == null)
+            {
+                return NotFound();
+            }
+
+            var place = await _context.Places.FindAsync(id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+            return View(place);
+        }
+
+        // POST: Places/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,State,City,Date")] Place place)
+        {
+            if (id != place.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(place);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlaceExists(place.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(place);
+        }
+
+        // GET: Places/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Places == null)
+            {
+                return NotFound();
+            }
+
+            var place = await _context.Places
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+
+            return View(place);
+        }
+
+        // POST: Places/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Places == null)
+            {
+                return Problem("Entity set 'DataContext.Places'  is null.");
+            }
+            var place = await _context.Places.FindAsync(id);
+            if (place != null)
+            {
+                _context.Places.Remove(place);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlaceExists(int id)
+        {
+          return (_context.Places?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
